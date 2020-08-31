39d2bf765fbb7dc282d04ab7ce00c18bddc6ee32
Alexandru Bejenaru <54982557+AlexBeje@users.noreply.github.com>
2020-08-31 20:56:52 +0200

    📦 NEW: Append the rectangles and the links to the graph

--- project-four/graph.js
+++ project-four/graph.js
@@ -1,4 +1,4 @@
-const dims = { height: 500, width: 1500 };
+const dims = { height: 500, width: 1100 };
 
 const svg = d3
   .select(".canvas")
@@ -9,25 +9,68 @@ const svg = d3
 const graph = svg.append("g").attr("transform", "translate(50, 50)");
 
 // tree and stratify
-const tree = d3.tree().size([dims.width, dims.height]);
-
 const stratify = d3
   .stratify()
   .id((d) => d.name)
   .parentId((d) => d.parent);
 
+const tree = d3.tree().size([dims.width, dims.height]);
+
 // update function
 const update = (data) => {
+  // remove current nodes
+  graph.selectAll(".node").remove();
+  graph.selectAll(".link").remove();
+
   // get updated root Node data
   const rootNode = stratify(data);
-  //const treeData = tree(rootNode);
+  const treeData = tree(rootNode).descendants();
 
-  console.log(rootNode);
-  //console.log(treeData);
+  // get nodes selection and join data
+  const nodes = graph.selectAll(".node").data(treeData);
 
-  // get nodes selection and join new data
-  // const nodes = graph.selectAll('.node')
-  //   .data(tree(rootNode).descendants());
+  // get link selection and join new data
+  const link = graph.selectAll(".link").data(tree(rootNode).links());
+
+  // enter new links
+  link
+    .enter()
+    .append("path")
+    .transition()
+    .duration(300)
+    .attr("class", "link")
+    .attr("fill", "none")
+    .attr("stroke", "#aaa")
+    .attr("stroke-width", 2)
+    .attr(
+      "d",
+      d3
+        .linkVertical()
+        .x((d) => d.x)
+        .y((d) => d.y)
+    );
+
+  // create enter node groups
+  const enterNodes = nodes
+    .enter()
+    .append("g")
+    .attr("class", "node")
+    .attr("transform", (d) => `translate(${d.x}, ${d.y})`);
+
+  // append rects to enter nodes
+  enterNodes
+    .append("rect")
+    .attr("fill", "#aaa")
+    .attr("stroke", "#555")
+    .attr("stroke-width", 2)
+    .attr("width", (d) => d.data.name.length * 20)
+    .attr("height", 50);
+
+  enterNodes
+    .append("text")
+    .attr("text-anchor", "middle")
+    .attr("fill", "white")
+    .text((d) => d.data.name);
 };
 
 // data & firebase hook-up
