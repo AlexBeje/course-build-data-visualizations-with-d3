3a7030b820dc16ba49ac16467fb9bc189bfcee7c
Alexandru Bejenaru <54982557+AlexBeje@users.noreply.github.com>
2020-08-31 19:39:18 +0200

    📦 NEW: Added the stratification of the data and the update function

--- project-four/graph.js
+++ project-four/graph.js
@@ -1,3 +1,35 @@
+const dims = { height: 500, width: 1500 };
+
+const svg = d3
+  .select(".canvas")
+  .append("svg")
+  .attr("width", dims.width + 100)
+  .attr("height", dims.height + 100);
+
+const graph = svg.append("g").attr("transform", "translate(50, 50)");
+
+// tree and stratify
+const tree = d3.tree().size([dims.width, dims.height]);
+
+const stratify = d3
+  .stratify()
+  .id((d) => d.name)
+  .parentId((d) => d.parent);
+
+// update function
+const update = (data) => {
+  // get updated root Node data
+  const rootNode = stratify(data);
+  //const treeData = tree(rootNode);
+
+  console.log(rootNode);
+  //console.log(treeData);
+
+  // get nodes selection and join new data
+  // const nodes = graph.selectAll('.node')
+  //   .data(tree(rootNode).descendants());
+};
+
 // data & firebase hook-up
 var data = [];
 
@@ -21,5 +53,5 @@ db.collection("employees").onSnapshot((res) => {
     }
   });
 
-  console.log(data);
+  update(data);
 });
